7c0c47e4272413bdb9c0ff7d36c4dff62a4b0c33
MamdouhSar <mamdouh.alsarayreh@gmail.com>
2017-07-04 13:06:04 +0300

    Created ConfirmationMessage component

---
+++ src/components/ConfirmationMessage.js
@@ -0,0 +1,20 @@
+/**
+ * Created by mamdouh on 04/07/17.
+ */
+import React, {Component} from 'react';
+
+class ConfirmationMessage extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <dialog open={false}>
+        test
+      </dialog>
+    );
+  }
+}
+
+export default ConfirmationMessage;
